95b439954e3b214b824502fb6326ec6177aa5d00
Nigel Bowden <wifinigel@gmail.com>
2020-06-27 15:21:53 +0100

    added route injection

--- wiperf_poller/testers/mgtconnectiontester.py
+++ wiperf_poller/testers/mgtconnectiontester.py
@@ -5,6 +5,8 @@ from socket import gethostbyname
 import requests
 
 from wiperf_poller.helpers.ethernetadapter import EthernetAdapter
+from wiperf_poller.helpers.route import check_correct_mgt_interface, inject_static_route
+from wiperf_poller.helpers.os_cmds import NC_CMD
 
 class MgtConnectionTester(object):
     """
@@ -24,14 +26,30 @@ class MgtConnectionTester(object):
         data_host = self.config_vars['data_host']
         data_port = self.config_vars['data_port']
 
+        # check if the route to the mgt server is over the correct interface...fix with route injection if not
+        if not check_correct_mgt_interface(data_host, self.config_vars, self.file_logger):
+
+            self.file_logger.warning("  We are not using the interface required for mgt traffic due to a routing issue in this unit - attempt route addition to fix issue")
+
+            if inject_static_route(data_host, self.config_vars, self.file_logger):
+
+                self.file_logger.info("  Checking if route injection worked...")
+
+                if check_correct_mgt_interface(data_host, self.config_vars, self.file_logger):
+                    self.file_logger.info("  Routing issue corrected OK.")
+                else:
+                    self.file_logger.warning("  We still have a routing issue. Will have to exit as mgt traffic over correct interface not possible")
+                    self.file_logger.warning("  Suggest making static routing additions or adding an additional metric to the interface causing the issue.")
+                    lockf_obj.delete_lock_file()
+                    sys.exit()
 
         # if we are using hec, make sure we can access the hec network port, otherwise we are wasting our time
         if data_transport == 'hec' and exporter_type == 'splunk':
-            self.file_logger.info("Checking port connection to Splunk server {}, port: {}".format(data_host, data_port))
+            self.file_logger.info("  Checking port connection to Splunk server {}, port: {}".format(data_host, data_port))
 
             try:
-                portcheck_output = subprocess.check_output('/bin/nc -zvw10 {} {}'.format(data_host, data_port), stderr=subprocess.STDOUT, shell=True).decode()
-                self.file_logger.info("Port connection to server {}, port: {} checked OK.".format(data_host, data_port))
+                portcheck_output = subprocess.check_output('{} -zvw10 {} {}'.format(NC_CMD, data_host, data_port), stderr=subprocess.STDOUT, shell=True).decode()
+                self.file_logger.info("  Port connection to server {}, port: {} checked OK.".format(data_host, data_port))
             except subprocess.CalledProcessError as exc:
                 output = exc.output.decode()
                 self.file_logger.error("Port check to server failed. Err msg: {} (Exiting...)".format(str(output)))
@@ -71,11 +89,11 @@ class MgtConnectionTester(object):
             return True
         
         elif exporter_type == 'influxdb':
-            self.file_logger.info("Checking port connection to InfluxDB server {}, port: {}".format(data_host, data_port))
+            self.file_logger.info("  Checking port connection to InfluxDB server {}, port: {}".format(data_host, data_port))
 
             try:
-                portcheck_output = subprocess.check_output('/bin/nc -zvw10 {} {}'.format(data_host, data_port), stderr=subprocess.STDOUT, shell=True).decode()
-                self.file_logger.info("Port connection to server {}, port: {} checked OK.".format(data_host, data_port))
+                portcheck_output = subprocess.check_output('{} -zvw10 {} {}'.format(NC_CMD, data_host, data_port), stderr=subprocess.STDOUT, shell=True).decode()
+                self.file_logger.info("  Port connection to server {}, port: {} checked OK.".format(data_host, data_port))
             except subprocess.CalledProcessError as exc:
                 output = exc.output.decode()
                 self.file_logger.error(
@@ -87,11 +105,11 @@ class MgtConnectionTester(object):
             return True
 
         elif exporter_type == 'influxdb2':
-            self.file_logger.info("Checking port connection to InfluxDB2 server {}, port: {}".format(data_host, data_port))
+            self.file_logger.info("  Checking port connection to InfluxDB2 server {}, port: {}".format(data_host, data_port))
 
             try:
-                portcheck_output = subprocess.check_output('/bin/nc -zvw10 {} {}'.format(data_host, data_port), stderr=subprocess.STDOUT, shell=True).decode()
-                self.file_logger.info("Port connection to server {}, port: {} checked OK.".format(data_host, data_port))
+                portcheck_output = subprocess.check_output('{} -zvw10 {} {}'.format(NC_CMD, data_host, data_port), stderr=subprocess.STDOUT, shell=True).decode()
+                self.file_logger.info("  Port connection to server {}, port: {} checked OK.".format(data_host, data_port))
             except subprocess.CalledProcessError as exc:
                 output = exc.output.decode()
                 self.file_logger.error(
@@ -103,7 +121,7 @@ class MgtConnectionTester(object):
             return True
         
         else:
-            self.file_logger.info("Unknown exporter type configured in config.ini: {} (exiting)".format(exporter_type))
+            self.file_logger.info("  Unknown exporter type configured in config.ini: {} (exiting)".format(exporter_type))
             sys.exit()
 
 

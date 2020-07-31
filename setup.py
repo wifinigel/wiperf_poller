951e445be9bb2b483cc37c646473259be5586a1f
Nigel Bowden <wifinigel@gmail.com>
2020-07-31 11:44:46 +0100

    move to v0.1.21

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="wiperf_poller",
-    version="0.1.20",
+    version="0.1.21",
     author="Nigel Bowden",
     author_email="wifinigel@gmail.com",
     description="Poller for the wiperf utlity",

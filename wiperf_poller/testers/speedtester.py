e754e5d43c2cbe56d33bd63a9f99b1181a035205
Nigel Bowden <wifinigel@gmail.com>
2020-12-28 20:58:23 +0000

    Remove dummy data option

--- wiperf_poller/testers/speedtester.py
+++ wiperf_poller/testers/speedtester.py
@@ -13,7 +13,7 @@ class Speedtester(object):
         self.file_logger = file_logger
 
 
-    def ooklaspeedtest(self, server_id='', DUMMY_DATA=False, DEBUG=False):
+    def ooklaspeedtest(self, server_id='', DEBUG=False):
         '''
         This function runs the actual speedtest and returns the result
         as a dictionary: 
@@ -59,55 +59,45 @@ class Speedtester(object):
                         'url': 'http://speedtest.unifone.net.nz/speedtest/upload.php'}]
         '''
 
-        if DUMMY_DATA == False:
-
-            # perform Speedtest
+        # perform Speedtest
+        try:
+            st = speedtest.Speedtest()
+        except Exception as error:
+            self.file_logger.error("Speedtest error: {}".format(error))
+            return False
+        # check if we have specific target server
+        if server_id:
+            self.file_logger.info("Speedtest info: specific server ID provided for test: {}".format(str(server_id)))
             try:
-                st = speedtest.Speedtest()
+                st.get_servers(servers=[server_id])
             except Exception as error:
-                self.file_logger.error("Speedtest error: {}".format(error))
+                self.file_logger.error("Speedtest error: unable to get details of specified server: {}, reason: {}".format(
+                    str(server_id), error))
                 return False
-            # check if we have specific target server
-            if server_id:
-                self.file_logger.info("Speedtest info: specific server ID provided for test: {}".format(str(server_id)))
-                try:
-                    st.get_servers(servers=[server_id])
-                except Exception as error:
-                    self.file_logger.error("Speedtest error: unable to get details of specified server: {}, reason: {}".format(
-                        str(server_id), error))
-                    return False
-            else:
-                try:
-                    st.get_best_server()
-                except Exception as error:
-                    self.file_logger.error("Speedtest error: unable to get best server, reason: {}".format(error))
-                    return False
-
-            try:
-                download_rate = '%.2f' % (st.download()/1024000)
-                self.file_logger.debug("Download rate = " + str(download_rate) + " Mbps")
-            except Exception as error:
-                self.file_logger.error("Download test error: {}".format(error))
-                return False
-
-            try:
-                upload_rate = '%.2f' % (st.upload(pre_allocate=False)/1024000)
-                self.file_logger.debug("Upload rate = " + str(upload_rate) + " Mbps")
-            except Exception as error:
-                self.file_logger.error("Upload test error: {}".format(error))
-                return False
-
-            results_dict = st.results.dict()
-            ping_time = int(results_dict['ping'])
-            server_name = results_dict['server']['host']
         else:
-            # create dummy data (for speed of testing)
-            import random
+            try:
+                st.get_best_server()
+            except Exception as error:
+                self.file_logger.error("Speedtest error: unable to get best server, reason: {}".format(error))
+                return False
 
-            ping_time = random.randint(20, 76)
-            download_rate = round(random.uniform(30.0, 60.0), 2)
-            upload_rate = round(random.uniform(3.0, 8.0), 2)
-            server_name = "dummy-speedtest2.warwicknet.com:8080"
+        try:
+            download_rate = '%.2f' % (st.download()/1024000)
+            self.file_logger.debug("Download rate = " + str(download_rate) + " Mbps")
+        except Exception as error:
+            self.file_logger.error("Download test error: {}".format(error))
+            return False
+
+        try:
+            upload_rate = '%.2f' % (st.upload(pre_allocate=False)/1024000)
+            self.file_logger.debug("Upload rate = " + str(upload_rate) + " Mbps")
+        except Exception as error:
+            self.file_logger.error("Upload test error: {}".format(error))
+            return False
+
+        results_dict = st.results.dict()
+        ping_time = int(results_dict['ping'])
+        server_name = results_dict['server']['host']
 
         self.file_logger.info('ping_time: {}, download_rate: {}, upload_rate: {}, server_name: {}'.format(
             ping_time, download_rate, upload_rate, server_name))

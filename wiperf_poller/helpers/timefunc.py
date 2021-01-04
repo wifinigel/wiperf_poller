cd910ef5b770bf3de3a1eda5259a2712cc25a686
Nigel Bowden <wifinigel@gmail.com>
2021-01-04 10:58:29 +0000

    Create funcs for misc time requirements

---
+++ wiperf_poller/helpers/timefunc.py
@@ -0,0 +1,52 @@
+"""
+Miscellaneous time functions
+"""
+
+import time
+from datetime import datetime
+import subprocess
+from wiperf_poller.helpers.os_cmds import TIMEDATECTL_CMD
+
+def time_synced():
+
+    # check if clock sync status is true from "timedatectl status" command
+    cmd = "{} status".format(TIMEDATECTL_CMD)
+    cmd_output = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True).decode()
+
+    for line in cmd_output.split('\n'):
+       if ("System clock" in line) and ("yes" in line):
+            return True
+    
+    return False
+
+def now_as_nsecs():
+    return int(time.time() * 1000000000)
+
+def now_as_usecs():
+    return int(time.time() * 1000000)
+
+def now_as_msecs():
+    return int(time.time() * 1000)
+
+def now_as_secs():
+    return int(time.time())
+
+def splunk_ts():
+    ts = datetime.utcnow().strftime('%m-%d-%Y %H:%M:%S.000 +00:00')
+    print(ts)
+    print(type(ts))
+    return ts
+
+def get_timestamp(config_vars):
+
+    if config_vars['time_format'] == "influxdb":
+        return now_as_msecs()
+
+    elif config_vars['time_format'] == "splunk":
+        return now_as_secs()
+    
+    elif config_vars['time_format'] == "influxdb2":
+        return now_as_msecs()
+    
+    else:
+        return now_as_secs()

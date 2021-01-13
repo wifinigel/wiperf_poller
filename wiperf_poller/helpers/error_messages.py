ef699c955e07fa607676833e4734a69f4be6bc0a
Nigel Bowden <wifinigel@gmail.com>
2021-01-13 17:10:43 +0000

    Modify info msgs to indicate if err msgs to send

--- wiperf_poller/helpers/error_messages.py
+++ wiperf_poller/helpers/error_messages.py
@@ -23,7 +23,7 @@ class ErrorMessages():
     def dump(self, exporter_obj):
 
         self.file_logger.info("####### poll error messages #######")
-        self.file_logger.info("Sending poll error messages to mgt platform")
+        
 
         # check if we have an error log
         if os.path.isfile(self.error_log_file):
@@ -42,6 +42,11 @@ class ErrorMessages():
             for line in lines:
                 if re.search(r'^\d\d\d\d-\d\d-\d\d', line) :
                     message_list.append(line.strip()[:150])
+            
+            if len(message_list) > 0:
+                self.file_logger.info("Sending poll error messages to mgt platform")
+            else:
+                self.file_logger.info("No error messages to dump")
 
             # limit to last n messages
             if len(message_list) > self.error_messages_limit:
@@ -65,8 +70,11 @@ class ErrorMessages():
                     return False
             
             return True
-       
-        self.file_logger.info("No error messages to dump")
+
+        else:
+            self.file_logger.error("No error log detected.")
+            return True
+            
 
 
 

c482832b7de4e5bde3d2822669ca4fb0b0e1e159
Nigel Bowden <wifinigel@gmail.com>
2021-01-16 19:46:16 +0000

    Move to 0.2.3 - fix spooling issue

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="wiperf_poller",
-    version="0.2.2",
+    version="0.2.3",
     author="Nigel Bowden",
     author_email="wifinigel@gmail.com",
     description="Poller for the wiperf utlity",

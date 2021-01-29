106946762849598fedc468d17a92ae7d288efc27
Nigel Bowden <wifinigel@gmail.com>
2021-01-29 15:21:09 +0000

    Add improved debugging of config pull

--- wiperf_poller/helpers/remoteconfig.py
+++ wiperf_poller/helpers/remoteconfig.py
@@ -13,6 +13,24 @@ import time
 ####################################
 # config server
 ####################################
+def anon_value(value):
+    """
+    Turn value in to anonymous value to protect sensitve data that will 
+    be printed in log info 
+
+    Args:
+        value (string): value to be anonymised
+    
+    Return:
+        Anonymised string
+    """
+    value = str(value)
+
+    if len(value) < 4:
+        return len(value) * "*"
+    else:
+        return value[0] + (len(value) -2) * "*" + value[-1]
+
 def read_remote_cfg(config_file, check_cfg_file, config_vars, file_logger):
     """
     Pull the remote cfg file if refresh time expired or on first boot
@@ -24,18 +42,26 @@ def read_remote_cfg(config_file, check_cfg_file, config_vars, file_logger):
     cfg_password = config_vars['cfg_password']
     cfg_text = ''
     
+    file_logger.debug("Trying to pull config file from URL: {}".format(cfg_file_url))
+
     # if we use a token, we need to set user/pwd to be token
     if cfg_token:
         cfg_username = cfg_token
         cfg_password = cfg_token
-    
-    file_logger.info("Trying to pull config file from : {}".format(cfg_file_url))
+        file_logger.debug("Credential used (token): {}".format(anon_value(cfg_token)))
+    else:
+        file_logger.debug("Credential used (user/pwd): {} / {}".format(anon_value(cfg_username), anon_value(cfg_password)))
+
     try:
         warnings.simplefilter('ignore',InsecureRequestWarning)
         response = requests.get(cfg_file_url, auth=(cfg_username, cfg_password), timeout=5)
         if response.status_code == 200:
-           cfg_text = response.text
-        file_logger.info("Config file pulled OK.")
+            cfg_text = response.text
+            file_logger.debug("HTTP reponse code: {}".format(response.status_code))
+            file_logger.info("Config file pulled OK.")
+        else:
+            file_logger.error("Config file pull failed: {} ({}) - Note: Pretty much everything fails as a 404 on GitHub (if that is your remote target), even auth issues".format(response.status_code, response.text))
+            return False
     except Exception as err:
         file_logger.error("Config file pull error:")
         file_logger.error("HTTP get error: {}".format(err))
@@ -89,9 +115,8 @@ def check_last_cfg_read(config_file, check_cfg_file, config_vars, file_logger):
             last_read_time = f.read()
         file_logger.debug("Last read timestamp: {}".format(last_read_time))
     except FileNotFoundError:
-        # file does not exist, create & write timestamp
-        file_logger.info("Timestamp file does not exist, creating...")
-        write_cfg_timestamp(check_cfg_file, file_logger)
+        # file does not exist
+        file_logger.info("Timestamp file does not exist yet - will be created after successful read from remote file store.")
     except Exception as e:
         file_logger.info("File read error: {}".format(e))
         return False
@@ -106,4 +131,3 @@ def check_last_cfg_read(config_file, check_cfg_file, config_vars, file_logger):
     else:
         file_logger.info("Not time to read remote cfg file.")
         return False
-

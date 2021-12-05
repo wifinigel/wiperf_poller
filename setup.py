6ff124a8469573c6e984a82216714a43f0de3b0b
Wifinigel <wifinigel@gmail.com>
2021-12-05 17:02:06 +0000

    bump ver to 0.3.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="wiperf_poller",
-    version="0.3.5",
+    version="0.3.6",
     author="Nigel Bowden",
     author_email="wifinigel@gmail.com",
     description="Poller for the wiperf utlity",
